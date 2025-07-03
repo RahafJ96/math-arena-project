0f26bb3b68bbe70615cdd60d6d2affe3e7f3bd35
unknown <rahafjazz@gmail.com>
2025-07-03 22:37:09 +0300

    game+ auth + database connected

--- game-service/server.js
+++ game-service/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const gameRoutes = require('./routes/game');
 
-dotenv.config();
-const app = express();
+require('dotenv').config();
 
-app.use(cors());
-app.use(express.json());
+const app = express();
+app.use(bodyParser.json());
+
+// Routes
 app.use('/game', gameRoutes);
 
 const PORT = process.env.PORT || 5000;
